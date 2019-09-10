1cf26cc5a5761bc253067d4ec20651fecc802e6e
Gabe <gilgabe214@gmail.com>
2019-09-09 21:05:14 -0400

    get content to display

---
+++ src/SocialMedia.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SocialMedia() {
+  return (
+    <div className="container social-media">
+      <div className="instagram">
+        <p>Instagram</p>
+      </div>
+      <div className="github">
+        <p>Github</p>
+      </div>
+      <div className="curensea">
+        <p>Curensea</p>
+      </div>
+    </div>
+  )
+}
+
+export default SocialMedia;
