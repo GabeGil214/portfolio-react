2a8ee3fbe45876ef050beb05844f948c4fa1f1c1
Gabe <gilgabe214@gmail.com>
2019-09-15 22:21:48 -0400

    move styles object into separate file

---
+++ src/assets/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  container: {
+    flexGrow: 1,
+    height: '90vh',
+    overflowX: 'hidden',
+  },
+  space: {
+    padding: '0 15px',
+  }
+
+})
+
+export default useStyles
